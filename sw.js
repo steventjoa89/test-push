bcf653add9d8025bf55583b5c8c35ec1b76a8179
Steven Tjoa <steventjoa89@gmail.com>
2021-09-21 03:53:01 +0700

    1212

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', function(e) {
   var options = {
-    "body": "<String>",
+    body: e.toString(),
     data: {
       dateOfArrival: Date.now(),
       primaryKey: '2'
