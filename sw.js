598342f319ae3c7d50db0524c9a2d2f4154e0ec4
Steven Tjoa <steventjoa89@gmail.com>
2021-09-21 03:47:47 +0700

    12

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('push', function(e) {
   var options = {
-    body: 'This body was generated from push',
+    "body": "<String>",
     data: {
       dateOfArrival: Date.now(),
       primaryKey: '2'
