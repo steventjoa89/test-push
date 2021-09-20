cb416a25a87353a6cc4cfdc4b560ab891a97df8a
Steven Tjoa <steventjoa89@gmail.com>
2021-09-21 03:46:42 +0700

    asd

--- sw.js
+++ sw.js
@@ -5,9 +5,20 @@ self.addEventListener('push', function(e) {
       dateOfArrival: Date.now(),
       primaryKey: '2'
     },
-
+    actions: [
+      {
+        action: 'explore',
+        title: 'Explore this new world',
+        icon: 'images/checkmark.png'
+      },
+      {
+        action: 'close',
+        title: 'Close',
+        icon: 'images/xmark.png'
+      }
+    ]
   };
   e.waitUntil(
     self.registration.showNotification('Hello World', options)
-  )
+  );
 });
